c5a67d035ebb43be891df23dd487ad43f08ef24c
Ji Eunyoung <jeunyeong323@gmail.com>
2024-02-14 21:33:25 +0900

    리뷰 페이지에 카테고리 추가

--- src/kr/co/softsoldesk/mapper/ReviewMapper.java
+++ src/kr/co/softsoldesk/mapper/ReviewMapper.java
@@ -4,14 +4,15 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import kr.co.softsoldesk.beans.ReviewBean;
 
 public interface ReviewMapper {
 
-	@Insert("insert into review(review_id, pro_id, user_id,  review_contents, rating, photos, review_date) "
-			+ "values(review_seq.nextval, #{pro_id, jdbcType=INTEGER, javaType=Integer}, #{user_id, jdbcType=INTEGER, javaType=Integer}, #{review_contents}, #{rating}, #{photos, jdbcType=VARCHAR}, sysdate)")
+	@Insert("insert into review(review_id, pro_id, user_id,  review_contents, rating, photos, review_date, category) "
+			+ "values(review_seq.nextval, #{pro_id, jdbcType=INTEGER, javaType=Integer}, #{user_id, jdbcType=INTEGER, javaType=Integer}, #{review_contents}, #{rating}, #{photos, jdbcType=VARCHAR}, sysdate, #{category})")
 	void addReviewInfo(ReviewBean writeReviewBean);
 	
 	@Select("select r.review_id, r.user_id, u.user_name AS writer_name, r.rating, r.review_contents, to_char(r.review_date,'YYYY-MM-DD') as review_date, r.photos "
@@ -43,6 +44,9 @@ public interface ReviewMapper {
 	@Delete("delete from review "
 			+ "where review_id = #{review_id}")
 	void deleteReview(int review_id);
+	
+	@Select("SELECT * FROM review LIMIT #{startIdx}, #{pageSize}")
+    List<ReviewBean> getReviewsPaged(@Param("startIdx") int startIdx, @Param("pageSize") int pageSize);
 	
 	
 	
